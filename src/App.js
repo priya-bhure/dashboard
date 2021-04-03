9b40ce88d1e69520e920f78df2e77af1476fd994
Priya Bhure <priyabhure7@gmail.com>
2021-04-03 23:37:20 +0530

    Update index.html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import SignInComponent from './components/login-component/login-component';
+import Categories from './components/categories/categories-component.js';
+import Category from './components/category/category-component.js';
+import AuthComponent from './components/auth-component/auth-component.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [userType, setUserType] = useState('');
+
+  return(<div className="app">
+          <Switch>
+              <Redirect exact from='/' to='/login' />
+              <Route
+                  exact
+                  path = "/login"
+                  render = { (props) => <SignInComponent {...props} setUserType = {setUserType} />}
+              /> 
+               <Route
+                    exact
+                    path = "/categories"
+                    render = {(props) => <AuthComponent {...props} isLoggedIn = {userType !== ''} ><Categories {...props} userType={userType} /></AuthComponent>}
+                >
+                </Route>
+                <Route
+                    exact
+                    path = "/categories/:id"
+                    render = {(props) => <AuthComponent {...props} setUserType = {setUserType} isLoggedIn = {userType !== ''} ><Category {...props} /></AuthComponent>}
+                /> 
+          </Switch>
+      </div>);
 }
 
 export default App;
